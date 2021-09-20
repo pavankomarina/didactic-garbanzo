1fa73d3c610cd937c6d52e28e3c0699808fe0e98
Pavan Kalyan <komarina.pavankalyan@gmail.com>
2021-09-20 23:01:35 +0530

    Add mongodb

--- djamongo/djamongo/settings.py
+++ djamongo/djamongo/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'djamongo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'djamongo',
     }
 }
 
